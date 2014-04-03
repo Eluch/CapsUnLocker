9089489de183e25e006287709152f509d00c3f77
Eluch <pkferi@gmail.com>
2014-04-03 22:45:46 +0200

    base commit

---
+++ CapsUnLocker/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CapsUnLocker
+{
+    /// <summary>
+    /// Automatics Caps Lock disabler
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Worker worker;
+        Thread thread;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            enableBox.Checked += enableBox_Checked;
+            enableBox.Unchecked += enableBox_Unchecked;
+            mainWindow.Closing += mainWindow_Closing;
+            worker = new Worker();
+        }
+
+        void enableBox_Checked(object sender, RoutedEventArgs e)
+        {
+            thread = new Thread(worker.run);
+            if (!thread.IsAlive)
+                thread.Start();
+        }
+
+        void enableBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (thread.IsAlive)
+                worker.stopGracefully();
+        }
+
+        void mainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (thread != null)
+                if (thread.IsAlive)
+                    worker.stopGracefully();
+        }
+    }
+}
---
+++ CapsUnLocker/Worker.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CapsUnLocker
+{
+    class Worker
+    {
+        [DllImport("user32.dll")]
+        static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, UIntPtr dwExtraInfo);
+
+        private bool shouldRun = false;
+
+        public Worker()
+        {
+        }
+
+        public void run()
+        {
+            shouldRun = true;
+            while (shouldRun)
+            {
+                if (Console.CapsLock)
+                {
+                    Thread.Sleep(350);
+                    const int KEYEVENTF_EXTENDEDKEY = 0x1;
+                    const int KEYEVENTF_KEYUP = 0x2;
+                    keybd_event(0x14, 0x45, KEYEVENTF_EXTENDEDKEY, (UIntPtr)0);
+                    keybd_event(0x14, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP,
+                    (UIntPtr)0);
+                }
+                Thread.Sleep(100);
+            }
+        }
+
+        public void stopGracefully()
+        {
+            shouldRun = false;
+        }
+    }
+}
