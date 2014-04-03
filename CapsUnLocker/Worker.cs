8d457c2f85ad705742cfea395fd4900b6903664b
Eluch <pkferi@gmail.com>
2014-04-03 22:56:35 +0200

    Addig some comments

--- CapsUnLocker/Worker.cs
+++ CapsUnLocker/Worker.cs
@@ -26,14 +26,14 @@ namespace CapsUnLocker
             {
                 if (Console.CapsLock)
                 {
-                    Thread.Sleep(350);
+                    Thread.Sleep(350); //I had to add this sleep, because without this the Caps Lock blinked..
                     const int KEYEVENTF_EXTENDEDKEY = 0x1;
                     const int KEYEVENTF_KEYUP = 0x2;
                     keybd_event(0x14, 0x45, KEYEVENTF_EXTENDEDKEY, (UIntPtr)0);
                     keybd_event(0x14, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP,
                     (UIntPtr)0);
                 }
-                Thread.Sleep(100);
+                Thread.Sleep(100); //This sleep needs for not to kill the processor.
             }
         }
 
